2aff88589b2e068ca5f22a19f9d13b75f421b2ce
Crowdbotics <team@crowdbotics.com>
2023-04-13 18:33:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "world-stage-music-39912",
+  url: "https://world-stage-music-39912.botics.co",
+  api: "https://world-stage-music-39912.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
